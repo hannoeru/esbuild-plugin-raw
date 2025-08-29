99add39c3453ae187cdbe0be0ed0c4bc308deb1e
Nicolas Gryman <ngryman@gmail.com>
2025-08-29 06:24:05 +0200

    fix: correctly handle imports with the same file name (#403)
    * fix: correctly handle imports with the same file name
    
    * chore: eslint format
    
    ---------
    
    Co-authored-by: hanlee <me@hanlee.co>

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
 import type { Plugin } from 'esbuild'
 import { readFile } from 'node:fs/promises'
-import path from 'node:path'
+import { join } from 'node:path'
 
 export default function rawPlugin(): Plugin {
   return {
     name: 'raw',
     setup(build) {
       build.onResolve({ filter: /\?raw$/ }, (args) => {
+        const resolvedPath = join(args.resolveDir, args.path)
         return {
-          path: args.path,
-          pluginData: {
-            isAbsolute: path.isAbsolute(args.path),
-            resolveDir: args.resolveDir,
-          },
+          path: resolvedPath,
           namespace: 'raw-loader',
         }
       })
-      build.onLoad({ filter: /\?raw$/, namespace: 'raw-loader' }, async (args) => {
-        const fullPath = args.pluginData.isAbsolute ? args.path : path.join(args.pluginData.resolveDir, args.path)
-        return {
-          contents: await readFile(fullPath.replace(/\?raw$/, '')),
-          loader: 'text',
-        }
-      })
+      build.onLoad(
+        { filter: /\?raw$/, namespace: 'raw-loader' },
+        async (args) => {
+          return {
+            contents: await readFile(args.path.replace(/\?raw$/, '')),
+            loader: 'text',
+          }
+        },
+      )
     },
   }
 }
